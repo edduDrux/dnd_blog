5ef9abea9c534fa3101a11cc6a145130f86a7477
Eduardo Sartori <eddudrux@gmail.com>
2025-02-13 00:12:50 -0300

    feat(content): atualiza componentes para incluir ID dinâmico e ajusta estilos responsivos

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,30 @@
 export const dataFake = [
     {
-        "id": 1,
+        "id": "1",
         "photo": "aaa",
-        "title": "saiu novas habilidades para warlocks",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque."
+        "photoCover": "poderosomago.avif",
+        "title": "Guia definitivo para Mestres Iniciantes no RPG de Mesa",
+        "description": "Dê seus primeiros passos como Mestre de RPG com confiança! Este guia essencial traz dicas práticas para criar campanhas envolventes, conduzir sessões dinâmicas e lidar com jogadores de forma eficiente."
     },
     {
-        "id": 2,
+        "id": "2",
         "photo": "aaa",
-        "title": "saiu novas habilidades para warlocks",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque."
+        "photoCover": "party.webp",
+        "title": "Os Erros mais comuns em RPG de Mesa (e Como Evitá-los)",
+        "description": "Evite armadilhas que podem atrapalhar sua experiência de jogo! Descubra os erros mais frequentes cometidos por jogadores e mestres, e aprenda estratégias para manter suas aventuras fluindo sem problemas."
+    },
+    {
+        "id": "3",
+        "photo": "aaa",
+        "photoCover": "warlock.jpg",
+        "title": "Dicas para Melhorar a Narração e Imersão no RPG",
+        "description": "Transforme suas sessões de RPG em verdadeiras histórias épicas! Aprenda técnicas para narrar de forma cativante, criar ambientações envolventes e prender a atenção dos jogadores do início ao fim."
+    },
+    {
+        "id": "4",
+        "photo": "aaa",
+        "photoCover": "yjljbzcpeam61.webp",
+        "title": "Como Criar Personagens Memoráveis para Suas Aventuras",
+        "description": "Construa personagens únicos e inesquecíveis! Descubra como dar profundidade, motivações e personalidade marcante aos seus heróis e vilões, tornando cada aventura ainda mais emocionante."
     }
 ]
